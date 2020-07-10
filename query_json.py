ab26fc4abfec5ae463ed6193eac155c80d392096
Hannah Friedman <hfriedman@Hannahs-MacBook-Pro-2.local>
2020-07-10 14:51:19 -0700

    initial commit

---
+++ query_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import requests
+import sys
+
+def main(args=None):
+
+   if len(args) == 0:
+       return 1
+   username = "read-c241f228d7e954631e800f3c75ca5921"
+   password = "u1PGZ8ZDPbxbogPDfSWoj+Rbbtg1twYZ+UiCoFuB"
+   api = 'https://api.ravelry.com' + args[0]
+   r = requests.get(api, auth=(username, password)) 
+   print(r.json())
+   return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
