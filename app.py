03eaa1fab17fb5884cc2b5e7aa2faf7a6cb4b0ab
Hannah Friedman <hannah@spottedsnake.net>
2020-08-01 10:35:18 -0700

    sorts patterns based on user preferences, but is not very elegant so feel free to change

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request
 from pattern_class import Pattern
 from feelings import Feelings
+from query_json import API
 import random
 
 app = Flask(__name__, template_folder = 'templates')
@@ -16,14 +17,14 @@ def settings():
             user_settings_dict[x] = request.form[x]
     return user_settings_dict
 def generate_query(settings_dict):
-    query = 'craft='+request.form['craft']
+    query = '/patterns/search.json?craft='+request.form['craft']+'&photo=yes'
     if 'free' in settings_dict.keys():
         if 'online' in settings_dict.keys():
             query = query + '&availability=free%7Conline%7Cravelry%2B-discontinued'
         else:
             query = query + '&availability=free%2B-discontinued'
     else:
-        query = query + 'availability=&%2B-discontinued'
+        query = query + '&availability=%2B-discontinued'
     for x in other_settings:
         if x in settings_dict.keys():
             query = query + '&' + x +'='+ request.form[x]
@@ -45,7 +46,11 @@ def button_function():
             hl = 0
         user.update_prefs(Pattern(request.form['id']), hl)
     finally:
-        pat = Pattern(random.randint(1,1000000))
+        try:
+            user_presets=generate_query(settings())
+        except: 
+            user_presets=request.form['user_presets']
+        pat = Pattern(random.choice(API().list_of_ids(user_presets)))
         try:
             if pat.free == True:
                 price_text = "This pattern is free!"
@@ -54,7 +59,7 @@ def button_function():
         except:
             price_text = 'No price listed.'
         return render_template("linked_image.html", id_num = int(pat.id), thumbnail = pat.thumbnail, url = pat.url, name = pat.name, notes = pat.notes, 
-                price = price_text, craft = pat.craft['name'], weight = pat.weight, downloadable = pat.downloadable, feels = generate_query(settings()))
+                price = price_text, craft = pat.craft['name'], weight = pat.weight, downloadable = pat.downloadable, user_presets = user_presets)
 
 if __name__ == '__main__':
     app.run()
